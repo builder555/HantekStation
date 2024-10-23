bdc3e446547a048b25dac8f22948f73128e1894d
builder555 <85308587+builder555@users.noreply.github.com>
2024-10-22 20:41:02 -0400

    fix: use new control component to display values

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,9 +1,5 @@
 <script setup>
-import { ref } from 'vue';
-import HalfGauge from './components/HalfGauge.vue'
-import Slider from '@vueform/slider';
-const voltage = ref(0);
-const maxVoltage = 160;
+import ValueControl from './components/ValueControl.vue';
 </script>
 
 <template>
@@ -12,29 +8,18 @@ const maxVoltage = 160;
   </header>
 
   <main>
-    <div class="control">
-      <HalfGauge 
+      <ValueControl
+        :max="160"
+        :coarseStep="1"
+        :fineStep="0.05"
         title="voltage"
-        :value="voltage" 
-        :maxValue="maxVoltage"
       />
-      <Slider
-        showTooltip="focus"
-        tooltipPosition="bottom"
-        :min="0"
-        :max="maxVoltage"
-        :lazy="false"
-        v-model="voltage"
-        style="width: 90%; --slider-height: 20px; --slider-handle-width: 34px; --slider-handle-height: 34px;"
-      />
-    </div>
-    <div class="control">
-      <HalfGauge 
+      <ValueControl
+        :max="4.1"
+        :coarseStep="0.1"
+        :fineStep="0.005"
         title="current"
-        :value="1.55" 
-        :maxValue="4.1"
       />
-    </div>
   </main>
 </template>
 
@@ -44,11 +29,6 @@ main {
   flex-direction: row;
   justify-content: space-evenly;
   align-items: flex-start;
-}
-.control {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 }
 </style>
 <style src="@vueform/slider/themes/default.css"></style>
