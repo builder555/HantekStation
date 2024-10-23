1f4fe7b49ad1d0777689b1f47f026a6fcb02dab7
builder555 <85308587+builder555@users.noreply.github.com>
2024-10-23 19:25:37 -0400

    feat: add on/off button to ui

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 import ValueControl from './components/ValueControl.vue';
 const voltage = ref(0);
 const current = ref(0);
+const isOn = ref(false);
 </script>
 
 <template>
@@ -21,6 +22,11 @@ const current = ref(0);
       title="current"
       v-model="current"
     />
+    <button
+      class="onoff"
+      :class="isOn ? 'on' : 'off'"
+      @click="isOn = !isOn"
+    >{{ isOn ? 'on' : 'off' }}</button>
   </main>
 </template>
 
@@ -29,7 +35,31 @@ main {
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
-  align-items: flex-start;
+  align-items: center;
 }
+.onoff {
+  font-size: 3em;
+  padding: 0;
+  width: 120px;
+  height: 120px;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+.onoff.on {
+  color: #4db1bc;
+  text-align: center;
+  animation-delay: 0.7s;
+  animation: glow 1s ease-in-out infinite alternate;
+}
+
+@keyframes glow {
+    from {
+      text-shadow: 0 0 30px #2d9da9;
+    }
+    to {
+      text-shadow: 0 0 40px #34b3c1, 0 0 20px #4dbbc7;
+    }
+  }
+
 </style>
 <style src="@vueform/slider/themes/default.css"></style>
