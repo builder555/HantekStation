e77e6548a026779c87233d731115b2ba2385c6df
builder555 <85308587+builder555@users.noreply.github.com>
2024-10-30 07:18:35 -0400

    feat: connect API to server

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,33 +1,77 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, computed, watch } from 'vue';
 import ValueControl from './components/ValueControl.vue';
+import { socket } from './websocket';
+const isInitialized = computed(() => maxVoltage.value > 0 && maxCurrent.value > 0 && socket.isInitialized);
 const voltage = ref(0);
 const current = ref(0);
 const isOn = ref(false);
+const maxVoltage = ref(0);
+const maxCurrent = ref(0);
+function togglePower() {
+  socket.send({
+    command: isOn.value ? 'POWER_OFF' : 'POWER_ON',
+  });
+}
+function throttle(fn, wait){
+    let throttled = false;
+    return function(...args){
+        if(!throttled){
+          throttled = true;
+          setTimeout(()=>{
+              fn.apply(this,args);
+              throttled = false;
+            }, wait);
+        }
+    }
+}
+watch(voltage, throttle(() => {
+  socket.send({
+    command: 'SET_VOLTAGE',
+    payload: voltage.value,
+  });
+}, 200));
+watch(current, throttle(() => {
+    socket.send({
+      command: 'SET_CURRENT',
+      payload: Math.round(current.value * 1000),
+    });
+}, 200));
+
+onMounted(async () => {
+  await socket.initSocket();
+  socket.on('POWER', v => isOn.value = v);
+  // socket.on('VOLTAGE', v => voltage.value = v);
+  // socket.on('CURRENT', v => current.value = v);
+  socket.on('MAX_VOLTAGE', v => maxVoltage.value = v);
+  socket.on('MAX_CURRENT', v => maxCurrent.value = v);
+});
 </script>
 
 <template>
-  <main>
+  <main v-if="isInitialized">
     <ValueControl
-      :max="160"
+      :max="maxVoltage"
       :coarseStep="1"
       :fineStep="0.05"
       title="voltage"
       v-model="voltage"
     />
     <ValueControl
-      :max="4.1"
+      :max="maxCurrent"
       :coarseStep="0.1"
       :fineStep="0.005"
       title="current"
       v-model="current"
     />
+  </main>
+  <footer>
     <button
       class="onoff"
       :class="isOn ? 'on' : 'off'"
-      @click="isOn = !isOn"
+      @click="togglePower"
     >{{ isOn ? 'on' : 'off' }}</button>
-  </main>
+  </footer>
 </template>
 
 <style scoped>
@@ -37,14 +81,16 @@ main {
   justify-content: space-evenly;
   align-items: center;
 }
+
 .onoff {
   font-size: 3em;
   padding: 0;
-  width: 120px;
-  height: 120px;
+  width: 100%;
+  height: 100px;
   text-transform: uppercase;
   font-weight: bold;
 }
+
 .onoff.on {
   color: #4db1bc;
   text-align: center;
@@ -53,13 +99,13 @@ main {
 }
 
 @keyframes glow {
-    from {
-      text-shadow: 0 0 30px #2d9da9;
-    }
-    to {
-      text-shadow: 0 0 40px #34b3c1, 0 0 20px #4dbbc7;
-    }
+  from {
+    text-shadow: 0 0 30px #2d9da9;
   }
 
+  to {
+    text-shadow: 0 0 40px #34b3c1, 0 0 20px #4dbbc7;
+  }
+}
 </style>
 <style src="@vueform/slider/themes/default.css"></style>
---
+++ ui/src/websocket.js
@@ -0,0 +1,55 @@
+class Socket {
+  constructor() {
+    const devPort = import.meta.env.WSPORT;
+    if (import.meta.env.MODE === 'production') {
+      this.port = window.location.port;
+    } else {
+      this.port = devPort ?? 8000;
+    }
+    this.socket = null;
+    this.callbacks = new Map();
+    this.callbacks.set('ERROR', this.defaultErrorHandler);
+  }
+  get isInitialized() {
+    return this.socket?.readyState === WebSocket.OPEN;
+  }
+  defaultErrorHandler(message) {
+    console.warn(message);
+  }
+  async initSocket() {
+    return new Promise(resolve => {
+      const host = window.location.hostname;
+      this.socket = new WebSocket(`ws://${host}:${this.port}/ws`);
+      this.setupMessageHandler();
+      this.socket.onopen = () => resolve();
+      this.socket.onerror = err => {
+        const callback = this.callbacks.get('ERROR');
+        callback('Error connecting to backend. Make sure the server is running.');
+        console.warn(err);
+      };
+    });
+  }
+  async ensureSocketConnected() {
+    if (this.isInitialized) {
+      await this.initSocket();
+    }
+  }
+  setupMessageHandler() {
+    this.socket.onmessage = message => {
+      const { payload, status, command } = JSON.parse(message.data);
+      const callback = this.callbacks.get(status === 'OK' ? command : 'ERROR');
+      if (callback) {
+        callback(payload);
+      }
+    };
+  }
+  async send(data) {
+    await this.ensureSocketConnected();
+    this.socket.send(JSON.stringify(data));
+  }
+  on(event, callback) {
+    this.callbacks.set(event, callback);
+  }
+}
+const socket = new Socket();
+export { socket };
