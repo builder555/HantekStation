a2a637d0a093a3c4d9d2a462fdd19f69026816d6
builder555 <85308587+builder555@users.noreply.github.com>
2024-10-30 07:17:28 -0400

    feat: first pass at server

---
+++ server/api/ws.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.staticfiles import StaticFiles
+from starlette.websockets import WebSocketState
+import json
+from hantekpsu import PSU
+
+DUMMY_DATA = True
+psu_limits = {
+    "HDP1160V4S": {
+        "MAX_VOLTAGE": 160,
+        "MAX_CURRENT": 4.1
+    }
+}
+
+app = FastAPI()
+psu = PSU()
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    model = "HDP1160V4S" if DUMMY_DATA else psu.get_model()
+    if model not in psu_limits:
+        await websocket.send_text(json.dumps({"status": "ERROR", "message": "Model not supported"}))
+        await websocket.close()
+        return
+    is_on = False if DUMMY_DATA else psu.get_on_off_status()
+    max_voltage = psu_limits[model]["MAX_VOLTAGE"]
+    max_current = psu_limits[model]["MAX_CURRENT"]
+    await websocket.send_text(json.dumps({"status": "OK", "command": "MAX_VOLTAGE", "payload": max_voltage}))
+    await websocket.send_text(json.dumps({"status": "OK", "command": "MAX_CURRENT", "payload": max_current}))
+    await websocket.send_text(json.dumps({"status": "OK", "command": "POWER", "payload": is_on}))
+    try:
+        while websocket.application_state == WebSocketState.CONNECTED:
+            message = await websocket.receive_text()
+            data = json.loads(message)
+            command = data['command']
+            if command == "POWER_ON":
+                psu.turn_on()
+                await websocket.send_text(json.dumps({"status": "OK", "command": "POWER", "payload": True}))
+            if command == "POWER_OFF":
+                psu.turn_off()
+                await websocket.send_text(json.dumps({"status": "OK", "command": "POWER", "payload": False}))
+            if command == "SET_VOLTAGE":
+                psu.set_output_voltage(data['payload'])
+            if command == "SET_CURRENT":
+                psu.set_output_current(data['payload'])
+    except WebSocketDisconnect:
+        print("WebSocket disconnected")
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
